14a5887dd1f02e5b18fc3112765aa656d48a9393
cursethrower <cursethrowermail@gmail.com>
2019-12-24 02:31:34 -0600

    Fix capitalization

--- bot.py
+++ bot.py
@@ -7,10 +7,9 @@ from discord.ext import commands
 # get data
 with open('./config/config.json') as f:
     config = json.load(f)
-token = config["token"]
+    token = config["token"]
 
 if not os.path.exists('./config/memory.json'):
-    print('doesn\'t exist')
     memory = dict()
     memory["spellbook"] = dict()
     with open('./config/memory.json', 'w') as f:
@@ -33,6 +32,6 @@ if __name__ == '__main__':
         try:
             bot.load_extension(cog)
         except Exception as error:
-            print(f'cog {cog} cannot be loaded. [{error}]')
+            print(f'Cog {cog} cannot be loaded. [{error}]')
 
     bot.run(token)
